7ef2b9968646d21c2f32704034345cae62fb5e3e
archi <kingll123m@mail.ru>
2024-05-15 16:56:00 +0500

    logic_verse

--- logic_plan.py
+++ logic_plan.py
@@ -62,7 +62,6 @@ def calculate_potentials(cost_matrix, initial_plan):
     return is_optimal(cost_matrix, initial_plan, u, v)
 
 
-
 def is_optimal(cost_matrix, initial_plan, u, v):
     num_rows = len(cost_matrix)
     num_cols = len(cost_matrix[0])
@@ -72,3 +71,83 @@ def is_optimal(cost_matrix, initial_plan, u, v):
                 if cost_matrix[i][j] - u[i] - v[j] < 0:
                     return False
     return True
+
+
+def redistribute(cost_matrix, initial_plan, u, v):
+    num_rows = len(cost_matrix)
+    num_cols = len(cost_matrix[0])
+
+    # Initialize closed list
+    closed = [[False] * num_cols for _ in range(num_rows)]
+
+    # Find cells to be closed
+    for i in range(num_rows):
+        for j in range(num_cols):
+            if initial_plan[i][j] > 0:
+                if not closed[i][j] and cost_matrix[i][j] - u[i] - v[j] != 0:
+                    closed[i][j] = True
+
+    # Continue until no more cells can be closed
+    while True:
+        # Find an unclosed cell
+        unclosed_cell = None
+        for i in range(num_rows):
+            for j in range(num_cols):
+                if not closed[i][j]:
+                    unclosed_cell = (i, j)
+                    break
+            if unclosed_cell:
+                break
+
+        if not unclosed_cell:
+            break  # No more unclosed cells
+
+        # Initialize the path with the unclosed cell
+        path = [unclosed_cell]
+        closed[unclosed_cell[0]][unclosed_cell[1]] = True
+
+        # Try to find a closed cell adjacent to the last cell in the path
+        while True:
+            current_cell = path[-1]
+            found_adjacent_closed = False
+
+            # Check adjacent cells
+            for di, dj in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                next_cell = (current_cell[0] + di, current_cell[1] + dj)
+
+                # Check if the next cell is within bounds
+                if 0 <= next_cell[0] < num_rows and 0 <= next_cell[1] < num_cols:
+                    if closed[next_cell[0]][next_cell[1]]:
+                        found_adjacent_closed = True
+                        break
+
+            if not found_adjacent_closed:
+                break  # No more adjacent closed cells
+
+            # Find the closed cell with the smallest marginal cost
+            min_marginal_cost = float('inf')
+            min_marginal_cost_cell = None
+            for i in range(num_rows):
+                for j in range(num_cols):
+                    if closed[i][j]:
+                        marginal_cost = cost_matrix[i][j] - u[i] - v[j]
+                        if marginal_cost < min_marginal_cost:
+                            min_marginal_cost = marginal_cost
+                            min_marginal_cost_cell = (i, j)
+
+            # Add the cell to the path and mark it as closed
+            path.append(min_marginal_cost_cell)
+            closed[min_marginal_cost_cell[0]][min_marginal_cost_cell[1]] = False
+
+        # Update the plan along the path
+        min_quantity = min(initial_plan[cell[0]][cell[1]] for cell in path)
+        for cell in path:
+            initial_plan[cell[0]][cell[1]] -= min_quantity
+
+        # Find the cell with the smallest quantity along the path
+        min_quantity_cell = min(path, key=lambda cell: initial_plan[cell[0]][cell[1]])
+
+        # Adjust the plan to satisfy the closed cells
+        initial_plan[min_quantity_cell[0]][min_quantity_cell[1]] += min_quantity
+
+    return initial_plan
